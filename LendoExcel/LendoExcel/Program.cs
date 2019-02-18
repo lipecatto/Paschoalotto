aa05f64f9768b2c4bf2299935860882fb733d7e1
fecatto <fecatto@MTZ-15762>
2019-02-18 16:18:53 -0300

    projeto excel

---
+++ LendoExcel/LendoExcel/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendoExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int arquivosPendentes = Directory.GetFiles(@"C:\ROBO\Encerramento\Pendentes", "*", SearchOption.AllDirectories).Length;
+
+                if (arquivosPendentes == 0)
+                {
+                    //Log nenhum arquivo
+                }
+                else if (arquivosPendentes == 1)
+                {
+                    string filepath = @"C:\ROBO\Encerramento\Pendentes";
+                    DirectoryInfo d = new DirectoryInfo(filepath);
+
+                    foreach (var file in d.GetFiles("*.csv"))
+                    {
+                        using (var reader = new StreamReader(file.FullName))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                var line = reader.ReadLine();
+                                var values = line.Split(';');
+
+                            }
+                        }
+                        Directory.Move(file.FullName, @"C:\ROBO\Encerramento\Processados\" + file.Name);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ LendoExcel/LendoExcel/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendoExcel
+{
+    class Utils
+    {
+        public static IWebElement WaitUntilElementExists(IWebDriver driver, By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                return null;
+            }
+        }
+    }
+}
